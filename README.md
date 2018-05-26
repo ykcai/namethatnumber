edfb1102393e68fc519b3147124b7933a94de40e
ykcai <lordsunday11@gmail.com>
2018-05-26 10:35:44 -0400

    Updated README Changed SimpleCNN

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # namethatnumber
+
 Handwritten Digit Recognition on the MNIST Dataset in Python using the Keras Deep Learning Library.
+
+# Input Number of features
+
+```python
+features = input("Number of features for each layer: ")
+```
+
+# Input the Optimizer Learning Rate
+
+```python
+learningRate = input("Learning rate of optimizer: ")
+```
+
+# Change the number of layers 1 to 3
+
+```python
+def baseline_model():
+	# create model
+	...
+		#remove this layer to get 2 layers
+	model.add(Conv2D(int(features), (3, 3), activation='relu'))
+	model.add(Flatten())
+		#remove this layer to get 1 layer
+	model.add(Dense(int(features), activation='relu'))
+  ...
+```
+
+# Run
+
+`python SimpleCNN.py` and enter the parameters.
--- SimpleCNN.py
+++ SimpleCNN.py
@@ -30,21 +30,27 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 # 10 classes (0-9 numbers)
 num_classes = y_test.shape[1]
-print("\n=====Training model with 5 features, 3 hidden layers, and learningRate = 0.001=====\n")
+features = input("Number of features for each layer: ")
+learningRate = input("Learning rate of optimizer: ")
+layers = 3;
+	#Change the numner of hidden layers before you run if things changed
+print("\n=====Training model with " + features + "input features, " + layers + " hidden layers, and learningRate = " + learningRate + "=====\n")
 def baseline_model():
 	# create model
 	model = Sequential()
     	#first argument of Conv2D is the # feature maps that are 5x5 and a rectifier ativation function
-	model.add(Conv2D(2, (5, 5), input_shape=(1, 28, 28), activation='relu'))
-	model.add(Conv2D(2, (3, 3), activation='relu'))
+	model.add(Conv2D(int(features), (5, 5), input_shape=(1, 28, 28), activation='relu'))
+		#remove this layer to get 2 layers
+	model.add(Conv2D(int(features), (3, 3), activation='relu'))
 	model.add(Flatten())
-	model.add(Dense(2, activation='relu'))
+		#remove this layer to get 1 layer
+	model.add(Dense(int(features), activation='relu'))
     	#output layer 10 neurons for 10 classes and a softmax activation function
     	#to ouput a probablity-like prediction for each class
 	model.add(Dense(num_classes, activation='softmax'))
 		# Compile model
 		# CHANGE LR (LEARNING RATE)
-	customOp = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+	customOp = optimizers.Adam(lr=float(learningRate), beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 	model.compile(loss='categorical_crossentropy', optimizer=customOp, metrics=['accuracy'])
 	return model
 
@@ -55,6 +61,8 @@ model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_s
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("CNN Error: %.2f%%" % (100-scores[1]*100))
+	#Change the numner of hidden layers before you run if things changed
+print("\n=====Training model with " + features + "input features, " + layers + " hidden layers, and learningRate = " + learningRate + "=====\n")
 
 # serialize model to JSON
 # model_json = model.to_json()
